ca3dfefb6653e7d0da05d4f268887e5ec4d52a47
Pentacloud-Consulting <shabahatsyed101@gmail.com>
2025-05-27 17:52:42 +0530

    Update vite.config.ts to fix axios dependency issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,9 +15,15 @@ export default defineConfig({
       }
     }
   },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    }
   },
 });
