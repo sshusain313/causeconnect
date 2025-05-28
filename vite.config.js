d297dfb8ea3626205218aee4a143aadf833a85f3
Pentacloud-Consulting <shabahatsyed101@gmail.com>
2025-05-28 16:51:19 +0530

    Update Vite configuration to use port 8085

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     host: "::",
-    port: 8080,
+    port: 8085,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
